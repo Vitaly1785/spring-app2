c55b98ec84f1cafc8820adc53e71773c1388fef3
Vitaly1785 <korsar1785@yandex.ru>
2021-02-13 18:14:05 +0300

    Initial commit

---
+++ src/main/java/ru/owncourseSpring/TestBean.java
@@ -0,0 +1,17 @@
+package ru.owncourseSpring;
+
+public class TestBean {
+    private String title;
+
+    public TestBean(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
